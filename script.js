b51a11348b2375c30039893a111a549a4c60740c
arizajulian77 <arizajulian955@gmail.com>
2026-05-19 22:24:38 -0500

    Implement student registration and display functions

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-console.log("Landing page funcionando");
+let estudiantes = JSON.parse(localStorage.getItem("estudiantes")) || [];
+
+mostrarRegistros();
+
+function registrar() {
+
+    let nombre = document.getElementById("nombre").value;
+    let documento = document.getElementById("documento").value;
+    let curso = document.getElementById("curso").value;
+
+    if(nombre === "" || documento === "" || curso === "") {
+
+        alert("Complete todos los campos");
+        return;
+
+    }
+
+    let estudiante = {
+
+        nombre,
+        documento,
+        curso
+
+    };
+
+    estudiantes.push(estudiante);
+
+    localStorage.setItem("estudiantes", JSON.stringify(estudiantes));
+
+    mostrarRegistros();
+
+    document.getElementById("nombre").value = "";
+    document.getElementById("documento").value = "";
+    document.getElementById("curso").value = "";
+
+}
+
+function mostrarRegistros() {
+
+    let lista = document.getElementById("registros");
+
+    lista.innerHTML = "";
+
+    estudiantes.forEach(estudiante => {
+
+        lista.innerHTML += `
+            <li>
+                ${estudiante.nombre} - ${estudiante.curso}
+            </li>
+        `;
+
+    });
+
+}
